d1d3b886987e4d1c8974f02819e0a44d5d4253d9
orfevre6 <icandoallthingsby30@gmail.com>
2020-06-10 11:37:39 +0000

    5章5.5.4終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
   
   def create
-    @user = User.new(params[:user])
+    @user = User.new(params[:user_patams])
     if @user.save
       
     else
